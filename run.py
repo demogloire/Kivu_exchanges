586967d09f872cc1efbeb740ee29944040961a29
Espoir Murhabazi <espoir.mur@gmail.com>
2020-01-15 22:23:23 +0200

    finish deployment with docker on localhost

--- run.py
+++ run.py
@@ -1,8 +1,13 @@
 import os
-
+from environs import Env
 from app import create_app
 
-config_name = os.getenv('FLASK_CONFIG')
+env = Env()
+env.read_env()
+
+config_name = env.str("FLASK_CONFIG", default='development')
+
+print('this is the config name ===>', config_name)
 app = create_app(config_name)
 
 if __name__ == '__main__':
