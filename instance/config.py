f7beb6d489a8e0cfe8eed53af398f31e2c1df96d
Espoir Murhabazi <espoir.mur@gmail.com>
2020-01-15 22:26:36 +0200

    remove instance config from github

--- instance/config.py
+++
@@ -1,3 +0,0 @@
-SECRET_KEY = '9462bfc3ca8d37b136173798873d05ea'
-SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://{user}:{password}@{server}/{database}'.format(user='root', password='gloire', server='localhost', database='kivuExchange')
-
